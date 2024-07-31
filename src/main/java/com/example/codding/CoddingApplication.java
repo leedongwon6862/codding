6bdefe38b5b8a5ce2e76cfd44fa399fe750b8c9e
leedongwon6862 <ckqkrhf6862@gmail.com>
2024-07-31 14:14:04 +0900

    basic_knowledge

---
+++ src/main/java/com/example/codding/CoddingApplication.java
@@ -0,0 +1,14 @@
+package com.example.codding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoddingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoddingApplication.class, args);
+	}
+
+
+}
